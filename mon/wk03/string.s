798e22c5180d402dc14886fb5e7f51aad70904a8
kingj2018 <jennifer.king652@gmail.com>
2022-10-14 11:09:13 +1100

    Update string.s

--- mon/wk03/string.s
+++ mon/wk03/string.s
@@ -5,17 +5,17 @@ main:   # length in $t0, s in $t1
         li	$t0, 0			# length = 0
         la	$t1, string		# load address of var1 into $t1# char *s = &string[0];
 loop:
-        # TODO: loop through all characters in the array to find the length
-        lb	$t2, ($t1)		# load byte from 0($s1) into $t1
+        # loop through all characters in the array to find the length
+        lb	$t2, ($t1)		# if (*s == 0) goto end_loop;
         beqz    $t2, end_loop
 
-        add     $t0, $t0, 1
-        add     $t1, $t1, 1     # s++
+        add     $t0, $t0, 1             # length++
+        add     $t1, $t1, 1             # s++
 
         b	loop			# branch to loop
 end_loop:
 
-        move	$a0, $t0		# a = $t1
+        move	$a0, $t0		# printf("%d\n", length);
         li      $v0, 1
         syscall
 
