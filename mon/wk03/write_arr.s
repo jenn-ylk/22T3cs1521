994e42542a649fc0f1ecd999728beb82db6a71ce
kingj2018 <jennifer.king652@gmail.com>
2022-10-14 11:37:47 +1100

    Update write_arr.s

--- mon/wk03/write_arr.s
+++ mon/wk03/write_arr.s
@@ -1,16 +1,16 @@
-# week 3 tutorial
+# week 3 tutorial - write numbers, from stdin into an array
 
 N_SIZE = 4
 
-main:
+main:   # i = $t0 , i * 4 = $t1
         li      $t0, 0
 loop1:
         bge     $t0, N_SIZE, end_loop1
-        # TODO: read integers from the command line and put them into the array
+        # read integers from the command line and put them into the array
         li      $v0, 5
         syscall
 
-        mul     $t1, $t0, 4
+        mul     $t1, $t0, 4             # multiply $t0 by 4 to get the correct offset for the index in numbers
         sw	$v0, numbers($t1)		
 
         add	$t0, $t0, 1
